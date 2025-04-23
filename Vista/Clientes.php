5a30f4960728b00d6fe807f036b88d0dcaced231
yosefv31 <yosefvargas25@gmail.com>
2025-04-23 16:48:51 -0600

    Update

--- Vista/Clientes.php
+++ Vista/Clientes.php
@@ -78,11 +78,5 @@ if (!isset($_SESSION['usuario'])) {
 
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-<!--Script para desabilitar botones-->
-<script>
-    document.getElementById("IngresoCliente").addEventListener("click", function () {
-        window.location.href = "index.php?controller=index&action=IngresoCliente";
-    });
-</script>
 </body>
 </html>
